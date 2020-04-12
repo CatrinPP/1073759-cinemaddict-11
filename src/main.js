b4133674232faa7f827d79330648a2ea10e6806e
CatrinPP <caterina.parshina@gmail.com>
2020-04-13 00:34:18 +0300

    доп блоки и кнопка showmore

--- src/main.js
+++ src/main.js
@@ -9,16 +9,19 @@ import {createFilmCardTemplate} from './components/film-card.js';
 import {createFooterStatisticsTemplate} from './components/footer-statistics.js';
 // import {createFilmDetailsTemplate} from './components/films-details.js';
 import {generateFilms} from './mock/film.js';
-import {FILM_CARDS_COUNT, FILM_CARDS_EXTRA_COUNT} from './const.js';
+import {FILM_CARDS_EXTRA_COUNT, SHOWING_CARDS_COUNT_ON_START, SHOWING_CARDS_COUNT_BY_BUTTON} from './const.js';
 import {getFilters, getWatchedFilmsCount} from './utils.js';
 
 const headerElement = document.querySelector(`.header`);
 const mainElement = document.querySelector(`.main`);
 const footerElement = document.querySelector(`.footer`);
+let showingFilmCardsCount = SHOWING_CARDS_COUNT_ON_START;
 
 const films = generateFilms();
 const filters = getFilters(films);
 const wathedFilmsCount = getWatchedFilmsCount(films);
+const filmsSortedByRating = films.slice().sort((a, b) => b.filmInfo.totalRating - a.filmInfo.totalRating);
+const filmsSortedByCommentsCount = films.slice().sort((a, b) => b.comments.length - a.comments.length);
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -40,19 +43,35 @@ const filmsListElement = filmsElement.querySelector(`.films-list`);
 
 render(filmsListElement, createShowMoreButton());
 
+const showMoreButton = filmsListElement.querySelector(`.films-list__show-more`);
 const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
-const filmsListExtraContainerElements = filmsElement.querySelectorAll(`.films-list--extra .films-list__container`);
+const filmsListExtraElements = filmsElement.querySelectorAll(`.films-list--extra`);
 
-const renderFilmCards = (container, count) => {
+showMoreButton.addEventListener(`click`, () => {
+  const prevFilmCardsCount = showingFilmCardsCount;
+  showingFilmCardsCount = showingFilmCardsCount + SHOWING_CARDS_COUNT_BY_BUTTON;
+
+  films.slice(prevFilmCardsCount, showingFilmCardsCount).forEach((card) => render(filmsListContainerElement, createFilmCardTemplate(card)));
+
+  if (showingFilmCardsCount >= films.length) {
+    showMoreButton.remove();
+  }
+});
+
+const renderFilmCards = (array, container, count) => {
   for (let i = 0; i < count; i++) {
-    render(container, createFilmCardTemplate(films[i]));
+    render(container, createFilmCardTemplate(array[i]));
   }
 };
 
-renderFilmCards(filmsListContainerElement, FILM_CARDS_COUNT);
+renderFilmCards(films, filmsListContainerElement, showingFilmCardsCount);
 
-filmsListExtraContainerElements.forEach((extraContainer) => {
-  renderFilmCards(extraContainer, FILM_CARDS_EXTRA_COUNT);
+filmsListExtraElements.forEach((element) => {
+  if (element.querySelector(`h2`).textContent === `Top rated`) {
+    renderFilmCards(filmsSortedByRating, element.querySelector(`.films-list__container`), FILM_CARDS_EXTRA_COUNT);
+  } else {
+    renderFilmCards(filmsSortedByCommentsCount, element.querySelector(`.films-list__container`), FILM_CARDS_EXTRA_COUNT);
+  }
 });
 
 // render(document.body, createFilmDetailsTemplate(films[0]));
